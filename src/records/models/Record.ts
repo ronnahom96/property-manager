20c6bc4b726eb250dcf8fbb4313df43a2bf34833
ronnahom96 <ronnahom96@gmail.com>
2023-03-26 01:03:26 +0300

    add date to the index

--- src/records/models/Record.ts
+++ src/records/models/Record.ts
@@ -3,13 +3,13 @@ import { IRecord } from '../interfaces';
 
 const recordSchema = new Schema<IRecord>({
     propertyId: { type: String, required: true },
-    prevPropertyValue: { type: Number, default: 0 },
+    balance: { type: Number },
     amount: { type: Number, required: true },
     date: { type: Date, required: true },
 });
 
 // Create index for read performance
-recordSchema.index({ propertyId: 1 });
+recordSchema.index({ propertyId: 1, date: -1 });
 
 export default model<IRecord>('Record', recordSchema);
 
