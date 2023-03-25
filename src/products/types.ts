01bf416c8e661460e812ca485953e2eb94e59314
ronnahom96 <ronnahom96@gmail.com>
2023-03-25 11:05:35 +0300

    delete not relevant files

--- src/products/types.ts
+++
@@ -1,6 +0,0 @@
-import { SelectQueryBuilder } from "typeorm";
-import { ProductFilter } from "../filters/ProductFilter";
-import { Product } from "./models/Product";
-
-export type ProductQueryBuilder = SelectQueryBuilder<Product>;
-export type ProductFilterList = ProductFilter[];
