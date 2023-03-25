4c4a56d53b7005c5debed8da8835ec191721fd16
ronnahom96 <ronnahom96@gmail.com>
2023-03-25 12:13:47 +0300

    delete redundant common interface

--- src/common/interfaces.ts
+++ src/common/interfaces.ts
@@ -9,14 +9,3 @@ export interface OpenApiConfig {
   jsonPath: string;
   uiPath: string;
 }
-
-export enum Operator {
-  GREATER = 'greater',
-  GREATER_EQUAL = 'greaterEqual',
-  LESS = 'less',
-  LESS_EQUAL = 'lessEqual',
-  EQUAL = 'equal',
-  CONTAINS = 'contains',
-  WITHIN = 'within',
-  INTERSECTS = 'intersects',
-}
