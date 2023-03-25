195fad9fa0f3e6c452705a318ddfba1e9c3ca325
ronnahom96 <ronnahom96@gmail.com>
2023-03-25 14:14:18 +0300

    improve constants

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -4,14 +4,13 @@ export const SERVICE_NAME = readPackageJsonSync().name ?? 'unknown_service';
 export const DEFAULT_SERVER_PORT = 80;
 
 // Errors
-export const NOT_FOUND = 'Not Found';
-export const INVALID_FILTER_TYPE = 'Invalid filter type';
+export const NOT_FOUND = 'No records found';
 
 /* eslint-disable @typescript-eslint/naming-convention */
 export const SERVICES: Record<string, symbol> = {
   CONFIG: Symbol('Config'),
   LOGGER: Symbol('Logger'),
-  ERROR_HANDLER: Symbol('ErrorHandle'),
+  ERROR_HANDLER: Symbol('ErrorHandler'),
   RECORD_MODEL: Symbol('RecordModel'),
 };
 /* eslint-enable @typescript-eslint/naming-convention */
